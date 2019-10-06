0a2a96fb1b5cf5a8cdb560e852512def491f9da5
pius712 <aksfbsgnlfls@gmail.com>
2019-10-06 14:05:20 +0900

    todoinput and todolist

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,15 +1,58 @@
 <template>
-    <div>
-        
+    <div class="inputBox">
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <!-- <button v-on:click="addTodo">add</button> -->
+        <span class="addContainer" v-on:click="addTodo" >
+            <i class="fas fa-plus addBtn"></i>
+        </span>
     </div>
 </template>
 
 <script>
 export default {
-
+    data: function(){
+        return{
+            newTodoItem:""
+        }
+    },
+    methods:{
+        addTodo: function(){
+            //  console.log(this.newTodoItem);
+             //저장하는 로직 
+             localStorage.setItem(this.newTodoItem, this.newTodoItem);
+             this.clearInput();
+        },
+        clearInput: function(){
+            this.newTodoItem ="";
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scope>
+input:focus{
+    outline:none;
+}
+.inputBox{
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+.inputBox input{
+    /* display: block; */
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer{
+    float:right;
+    display: block;
+    width:3rem;
+    border-radius: 0 5px 0 5px;
+}
+.addBtn{
+    /* display: block; */
+    /* color:white; */
+    vertical-align: middle;
+}
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+    <!-- ul>li*3 -->
+    <ul>
+        <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+            {{todoItem}}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            todoItems:[],
+        }
+    },
+    created: function(){
+        // 생성되는 순간 호출되는 
+        if(localStorage.length>0){
+            for(var i=0; i<localStorage.length; i++){
+                this.todoItems.push(localStorage.key(i));           
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
