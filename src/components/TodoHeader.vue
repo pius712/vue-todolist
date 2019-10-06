94201d2f6f69e1e082d34a52f7a20d220bf05f0a
pius712 <aksfbsgnlfls@gmail.com>
2019-10-06 12:29:02 +0900

    first commit

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <h1>Todo List</h1>
+    </header>
+</template>
+
+<script>
+export default {
+
+
+//js script
+}
+</script>
+
+<style scoped>
+/* css */
+h1{
+    color:grey;
+    font-weight: 980;
+    margin: 2.5rem 0 1.5rem
+
+}
+</style>
