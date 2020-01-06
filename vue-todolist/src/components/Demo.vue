acd36707120825b76c886999939e94576f8a9197
pius712 <aksfbsgnlfls@gmail.com>
2020-01-06 21:25:46 +0900

    test

---
+++ vue-todolist/src/components/Demo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="root">
+      <!-- <p>{{ this.$store.getters.originalPrice }}</p>
+      <p>{{ this.$store.getters.dobulePrice }}</p>
+      <p>{{ this.$store.getters.triplePrice }}</p> -->
+      <p>{{ originalPrice }}</p>
+      <p>{{ doublePrice }}</p>
+      <p>{{ triplePrice }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+    computed:{
+        ...mapGetters(['originalPrice','doublePrice','triplePrice'])
+        // originalPrice(){
+        //     return this.$store.getters.originalPrice;
+        // },
+        // doublePrice(){
+        //     return this.$store.getters.doublePrice;
+        // },
+        // triplePrice(){
+        //     return this.$store.getters.triplePrice;
+        // }
+    }
+}
+</script>
+
+<style>
+
+</style>
